e4d17005dbfad9bcd8be57423d37c4b564b6b94a
codernaut <gul.zaib@gmail.com>
2017-04-23 20:43:25 +0500

    checkpoint
    frequest checkins are necessary cause of my two year old daughter taking
    over my main work pc and I had to sycn to secondry work laptop

--- JavaSSHUtil/src/com/gul/onion/util/ssh/TestBasicSSH.java
+++ JavaSSHUtil/src/com/gul/onion/util/ssh/TestBasicSSH.java
@@ -1,9 +1,41 @@
 package com.gul.onion.util.ssh;
 
-public class TestBasicSSH {
+import com.jcabi.ssh.Shell;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import com.jcabi.ssh.SSH;
+
+/**
+ * 
+ * Basic ssh test class
+ * @author Gul Zaib
+ *
+ */
+public class TestBasicSSH {
+	
+	/**
+	 * test code from
+	 * http://ssh.jcabi.com/
+	 * @param args
+	 */
+
+	private static final String host="";
+	private static final int port=22;
+	private static final String userName="root";
+	private static final String password ="test"
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Shell shell;
+		try {
+			shell = new SSH(host, port, userName, password);
+			String stdout = new Shell.Plain(shell).exec("echo 'Hello, world!'");
+		} catch ( IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 	}
 
